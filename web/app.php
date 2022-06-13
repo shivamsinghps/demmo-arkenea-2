e18b36b1ea530ef0ab1f0e430a00c3c878f87088
Rahul Pandey <rrkrp100@gmail.com>
2022-06-13 22:06:18 +0530

    deployed on ec2

--- web/app.php
+++ web/app.php
@@ -14,13 +14,13 @@ $kernel = new AppKernel(APPLICATION_ENV, APPLICATION_DEBUG);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
-if (APPLICATION_ENV == 'prod') {
-    if (class_exists('AppCache')) {
-        $kernel = new AppCache($kernel);
+// if (APPLICATION_ENV == 'prod') {
+//     if (class_exists('AppCache')) {
+//         $kernel = new AppCache($kernel);
 
-        Request::enableHttpMethodParameterOverride();
-    }
-}
+//         Request::enableHttpMethodParameterOverride();
+//     }
+// }
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
