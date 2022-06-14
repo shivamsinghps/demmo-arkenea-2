1aa929752c6c81ae70f7b3659a13f8a36174c903
Rahul Pandey <rrkrp100@gmail.com>
2022-06-14 11:21:42 +0530

    configuration updated

--- web/app.php
+++ web/app.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('APPLICATION_DEBUG') or define('APPLICATION_DEBUG', getenv('APPLICATION_DEBUG') === '1');
-defined('APPLICATION_ENV') or define('APPLICATION_ENV', getenv('APPLICATION_ENV') ?: 'dev');
+defined('APPLICATION_ENV') or define('APPLICATION_ENV',  'dev');
 
 use Symfony\Component\HttpFoundation\Request;
 
@@ -14,13 +14,13 @@ $kernel = new AppKernel(APPLICATION_ENV, APPLICATION_DEBUG);
 if (PHP_VERSION_ID < 70000) {
     $kernel->loadClassCache();
 }
-// if (APPLICATION_ENV == 'prod') {
-//     if (class_exists('AppCache')) {
-//         $kernel = new AppCache($kernel);
+if (APPLICATION_ENV == 'prod') {
+    if (class_exists('AppCache')) {
+        $kernel = new AppCache($kernel);
 
-//         Request::enableHttpMethodParameterOverride();
-//     }
-// }
+        Request::enableHttpMethodParameterOverride();
+    }
+}
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
